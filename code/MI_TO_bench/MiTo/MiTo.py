5aadd67f7ef11c63032a04c6876b4d16d6b9594a
Andrea Cossa <andrea.cossa05@gmail.com>
2025-01-23 14:55:02 +0100

    MiTo plot distributions

--- code/MI_TO_bench/MiTo/MiTo.py
+++ code/MI_TO_bench/MiTo/MiTo.py
@@ -3,13 +3,9 @@ MiTo.
 """
 
 import os
-from itertools import product
 from mito_utils.utils import *
 from mito_utils.preprocessing import *
-from mito_utils.plotting_base import *
-from mito_utils.metrics import *
-from mito_utils.dimred import *
-from mito_utils.phylo import *
+from plotting_utils._plotting_base import *
 matplotlib.use('macOSX')
 
 
@@ -17,14 +13,12 @@ matplotlib.use('macOSX')
 
 
 # Args
-os.chdir('/Users/IEO5505/Desktop/MI_TO/mito_utils/scratch')
+path_afm = '/Users/IEO5505/Desktop/MI_TO/MI_TO_analysis_repro/data/MI_TO_bench/AFMs/maegatk/MDA_PT/afm.h5ad'
 path_dbSNP = '/Users/IEO5505/Desktop/MI_TO/MI_TO_analysis_repro/data/MI_TO_bench/miscellanea/dbSNP_MT.txt'
 path_REDIdb = '/Users/IEO5505/Desktop/MI_TO/MI_TO_analysis_repro/data/MI_TO_bench/miscellanea/REDIdb_MT.txt'
 
-afm = sc.read('afm.h5ad')
-afm = filter_cells(afm, cell_filter='None')
-afm_raw = afm.copy()
-
+afm = sc.read(path_afm)
+afm = filter_cells(afm, cell_filter='filter_2')
 afm = filter_afm(
     afm,
     min_cell_number=10,
@@ -37,11 +31,11 @@ afm = filter_afm(
         'min_n_positive' : 5,
         'af_confident_detection' : .03,
         'min_n_confidently_detected' : 2,
-        'min_mean_AD_in_positives' : 1.5,       # 1.25,
+        'min_mean_AD_in_positives' : 1.25,       # 1.25,
         'min_mean_DP_in_positives' : 5
     },
     binarization_kwargs={
-        't_prob':.9, 't_vanilla':.0, 'min_AD':2, 'min_cell_prevalence':.05
+        't_prob':.7, 't_vanilla':.0, 'min_AD':2, 'min_cell_prevalence':.05
     },
     bin_method='MiTo',
     path_dbSNP=path_dbSNP, 
@@ -50,16 +44,6 @@ afm = filter_afm(
 )
 
 compute_distances(afm, precomputed=True)
-tree = build_tree(afm, precomputed=True)
-tree, _,_ = MiToTreeAnnotator(tree)
-df = tree.cell_meta.dropna()
-ari = custom_ARI(df['GBC'], df['MT_clone'])
-nmi = normalized_mutual_info_score(df['GBC'], df['MT_clone'])
-ari
-nmi
-
-calculate_corr_distances(tree)
-
 np.sum(afm.layers['AD'].A, axis=0)
 np.sum(afm.layers['site_coverage'].A>0, axis=0)
 
@@ -97,33 +81,33 @@ plt.show()
 var_order = np.argsort((afm.layers['AD'].A).mean(axis=0))
 afm.var_names[var_order][-10:]
 
-mut = '15876_T>C'
+# mut = '15876_T>C'
+# mut = '9498_T>C'
+mut = '9528_C>T'
 ad = afm[:,mut].layers['AD'].A.flatten()
 dp = afm[:,mut].layers['site_coverage'].A.flatten()
-
 ad0, ad1 = get_components(ad[dp>0], dp[dp>0])
 
 
 ##
 
-afm.var
 
-fig, ax = plt.subplots(figsize=(4,4))
-
-colors = {'Observed':'k', 'C0':'orange', 'C1':'darkred'}
-sns.kdeplot(ad[dp>0], ax=ax, bw_adjust=3, color=colors['Observed'], fill=True, alpha=.2)
-sns.kdeplot(ad0, ax=ax, bw_adjust=3, color=colors['C0'], fill=True, alpha=.2)
-sns.kdeplot(ad1, ax=ax, color=colors['C1'], fill=True, alpha=.2)
-add_legend(ax=ax, colors=colors, loc='upper right', label='Distribution', bbox_to_anchor=(1,1), ticks_size=9, artists_size=10, label_size=10)
+fig, ax = plt.subplots(figsize=(5,4))
+colors = {'Observed':'k', 'C0':'#2EA867', 'C1':'#1D6C42'}
+sns.kdeplot(ad[dp>0], ax=ax, color=colors['Observed'], bw_adjust=3, fill=True, alpha=.2)
+sns.kdeplot(ad0, ax=ax, bw_adjust=5, color=colors['C0'], fill=True, alpha=.2)
+sns.kdeplot(ad1, ax=ax, color=colors['C1'], bw_adjust=3, fill=True, alpha=.2)
+add_legend(ax=ax, colors=colors, loc='upper right', label='Distribution', 
+           bbox_to_anchor=(1,1), ticks_size=9, artists_size=10, label_size=10)
+ax.set(xlabel='n of alternative UMIs')
+ax.spines[['right', 'top']].set_visible(False)
 fig.tight_layout()
 plt.show()
 
 #
 
 df = genotype_mix(ad, dp, t_prob=.7, min_AD=2, debug=True)
-
-
-df[df['geno_vanilla'] != df['geno_prob']]
+df[df['geno_vanilla'] == df['geno_prob']]
 
 
 ##

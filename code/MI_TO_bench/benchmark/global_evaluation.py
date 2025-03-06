b021b91c3af6be8cf403194a6a22f41e7df01506
Andrea Cossa <andrea.cossa05@gmail.com>
2025-03-06 18:26:57 +0100

    change to global evaluation and MiTo vs others

--- code/MI_TO_bench/benchmark/global_evaluation.py
+++ code/MI_TO_bench/benchmark/global_evaluation.py
@@ -17,14 +17,15 @@ matplotlib.use('macOSX')
 path_main = '/Users/IEO5505/Desktop/MI_TO/MI_TO_analysis_repro'
 path_data = os.path.join(path_main, 'data', 'MI_TO_bench', 'AFMs')
 # path_results = os.path.join(path_main, 'results', 'MI_TO_bench', 'benchmark', 'tuning', 'last_run_for_thesis')
-path_results = os.path.join(path_main, 'results', 'MI_TO_bench', 'benchmark1')
+path_results = os.path.join(path_main, 'results', 'MI_TO_bench', 'benchmark_final')
 
 
 ##
 
 
 # Set annot
-groupings = ['pp_method', 'bin_method', 'af_confident_detection', 'min_n_confidently_detected', 'min_AD']
+# groupings = ['pp_method', 'bin_method', 'af_confident_detection', 'min_n_confidently_detected', 'min_AD']
+groupings = 'job_id'
 metric_annot = {
     'Mutation Quality' : ['n_dbSNP', 'n_REDIdb', 'transitions_vs_transversions_ratio'],
     'Association with GBC' : ['freq_lineage_biased_muts', 'AUPRC', 'ARI', 'NMI'],                               
@@ -57,12 +58,21 @@ options = pd.read_csv(os.path.join(path_results, 'options.csv')).iloc[:,0].to_li
 options += ['pp_method']
 df = df.drop(columns=['median_target/untarget_coverage_logratio'])
 
-# One sample/task
-sample = 'AC_AC_PTs_1'
-df = df.query('sample==@sample')
+# Explore
+df.groupby('sample')[['corr', 'n_cells', 'n_GBC_groups', 'n_vars', 'ARI', 'NMI']].describe().T
 
-# Explore ...
-# ...
+# One sample/task
+sample = 'MDA_lung'
+df = df.query('sample==@sample')
+df['perc_unassigned'] = df['unassigned'] / df['n_cells']
+df['delta_GBC_groups'] = np.abs(df['n_GBC_groups'] - df['n MiTo clone'])
+(
+    df.query('n_cells>=1000 and n_GBC_groups>=9 and n_vars>10')
+    [['ARI', 'corr', 'NMI', 'AUPRC', 'n_cells', 'n_vars', 'n_GBC_groups', 'n MiTo clone', 'perc_unassigned', 'delta_GBC_groups']]
+)
+
+# df.sort_values('ARI', ascending=False).head(50)[['ARI', 'corr', 'NMI', 'AUPRC', 'n_cells', 'n_vars']]
+# df.query('AUPRC>.25 and corr>.25 and n_cells>=850 and n_GBC_groups>=9 and n_vars>10')[['ARI', 'corr', 'NMI', 'AUPRC', 'n_cells', 'n_vars']]
 
 # Score and rank, single task
 # n = 5
@@ -75,22 +85,32 @@ df = df.query('sample==@sample')
 
 # Options of interests
 df_selected = (
-    df.query('corr>.5 and n_cells>1000 and n_GBC_groups>30 and n_vars>10') 
-    [['job_id', 'pp_method', 'bin_method', 'af_confident_detection', 'min_AD', 'ARI', 'NMI', 'corr', 'AUPRC', 'freq_lineage_biased_muts', 'n_cells', 'n_vars', 'n_GBC_groups', 'mean_CI']]
+    df.query('n_cells>=1000 and n_GBC_groups>=9 and n_vars>10')
+    [[
+        'job_id', 'pp_method', 'bin_method', 'af_confident_detection',
+        'ARI', 'corr', 'NMI', 'AUPRC', 'n_cells', 'n_vars', 'n_GBC_groups', 'n MiTo clone', 'perc_unassigned', 'delta_GBC_groups'
+    ]]
+    .sort_values('ARI', ascending=False)
+    .head(5)
 )
-df_selected['cat'] = pd.cut(df_selected['n_vars'], bins=5)
-df_selected.groupby('cat').size() 
-df_selected = df_selected.groupby('cat').apply(lambda x: x.sort_values('corr', ascending=False).head(2)).reset_index(drop=True)
-df_selected
+
+# df_selected['cat'] = pd.cut(df_selected['n_vars'], bins=3)
+# df_selected.groupby('cat').size() 
+# df_selected = df_selected.groupby('cat').apply(lambda x: x.sort_values('ARI', ascending=False).head(2)).reset_index(drop=True)
+# df_selected[['NMI', 'ARI']].describe()
 
 # Write out
+path_ = '/data/cossa2_dare/MI_TO_bench/data/AFMs'
 # L = []
-# for i in range(df_selected.shape[0]):
-#     l = [sample, os.path.join(path_data, df_selected['pp_method'].values[i], sample, 'afm.h5ad'), df_selected['job_id'].values[i], "None"]
-#     L.append(l)
-# 
-# pd.DataFrame(L, columns=['sample', 'ch_matrix', 'job_id', 'cell_file']).set_index('sample').to_csv(os.path.join(path_results, f'{sample}_final_jobs.csv'))
+for i in range(df_selected.shape[0]):
+    l = [df_selected['job_id'].values[i], sample, os.path.join(path_, df_selected['pp_method'].values[i], sample, 'afm.h5ad')]
+    L.append(l)
 
+(
+    pd.DataFrame(L, columns=['job_id', 'sample', 'ch_matrix'])
+    .set_index('job_id')
+    .to_csv(os.path.join(path_results, 'final_jobs.csv'))
+)
 
 ##
 
@@ -101,12 +121,14 @@ df_selected
 
 
 
-
-
-
-
-
 #### REMINDERs
+
+
+# FILTER USED!! MiTo Tree annotator final refactoring, AFMs
+# 'AUPRC>=.25 and corr>=.25 and n_cells>=250(850) and n_GBC_groups>=6(8,30) and n_vars>=10'), ranked by ARI
+
+# FILTER USED!!, All benchmark mito_preprocessing and maegatk. Before MiTo Tree annotator final refactoring
+# AC_AC_PTs_1: df.query('corr>.5 and n_cells>1000 and n_GBC_groups==9 and n_vars>10')
 
 # FILTER USED!!, THESIS
 # MDA_clones: df.query('AUPRC>.5 and corr>.6 and n_cells>300 and n_GBC_groups==7 and n_vars>10')    
--- code/MI_TO_bench/phylo_inference/MiTo_vs_others.py
+++ code/MI_TO_bench/phylo_inference/MiTo_vs_others.py
@@ -1,5 +1,5 @@
 """
-Is MI-TO better at solving clones than other clonal reconstruction approaches?
+Is MiTo better at solving clones than other clonal reconstruction approaches?
 """
 
 import os
@@ -15,14 +15,9 @@ matplotlib.use('macOSX')
 
 # Get metrics
 path_main = '/Users/IEO5505/Desktop/MI_TO/MI_TO_analysis_repro'
-path_data = os.path.join(path_main, 'results', 'MI_TO_bench', 'phylo_inference', 'NJ')
-path_results = os.path.join(path_main, 'results', 'MI_TO_bench', 'phylo_inference')
+path_data = os.path.join(path_main, 'results', 'MI_TO_bench', 'phylo_inference_final', 'refining_mito_maegatk')
+path_results = os.path.join(path_main, 'results', 'MI_TO_bench', 'phylo_inference_final')
 path_colors = os.path.join(path_main, 'data/MI_TO_bench/clones_colors_sc.pickle')
-
-
-
-len(list(os.walk(path_data)))
-
 
 # Extract data
 tree_metrics = []
@@ -30,11 +25,11 @@ bench_results = []
 for folder, sub, files in os.walk(path_data):
     for file in files:
         splitted = os.path.join(folder, file).split('/')
-        method = splitted[-4]
+        # method = splitted[-4]
         sample = splitted[-3]
         job_id = splitted[-2]
         if file.startswith('tree'):
-            tree_metrics.append(pd.read_csv(os.path.join(folder, file), index_col=0).assign(method=method, sample=sample))
+            tree_metrics.append(pd.read_csv(os.path.join(folder, file), index_col=0).assign(sample=sample)) # method=method
         elif file.startswith('bench'):
             with open(os.path.join(folder, file), 'rb') as f:
                 d = pickle.load(f)
@@ -46,47 +41,44 @@ for folder, sub, files in os.walk(path_data):
             res['job_id'] = [ job_id for _ in range(len(d)) ]
             res['method'] = list(d.keys())
             bench_results.append(pd.DataFrame(res))
+
+# MiTo metrics           
 df_metrics = (
     pd.concat(tree_metrics)
-    .pivot_table(values='value', columns='metric', index=['method', 'sample', 'job_id'])
+    .pivot_table(values='value', columns='metric', index=['sample', 'job_id'])
     .reset_index()
 )
-df_metrics.groupby(['method']).median().T
+df_metrics.groupby('sample').median().T
+
+# MiTo vs others bench
 df_bench = pd.concat(bench_results)
-
-df_bench.groupby(['sample', 'method']).mean()
-
-# Read tables
-df_metrics = pd.read_csv(os.path.join(path_data, 'metrics.csv'), index_col=0)
-df_metrics['method'] = df_metrics['method'].map(lambda x: f'MiTo-{x}')
-df_bench = pd.read_csv(os.path.join(path_data, 'bench_df.csv'), index_col=0)
-cols = ['sample', 'job_id', 'ARI', 'NMI', 'method']
-df_bench = pd.concat([ df_bench.query('method!="MI_TO"')[cols], df_metrics[cols] ])
+df_bench.groupby(['sample', 'method']).median()
 
 
 ##
 
 
 # Viz performance
-fig, axs = plt.subplots(1,2,figsize=(10,4))
+fig, axs = plt.subplots(1,2,figsize=(9,4))
 
 df_bench['sample'] = pd.Categorical(df_bench['sample'], categories=['MDA_clones', 'MDA_lung', 'MDA_PT'])
 order = df_bench.groupby('method')['ARI'].median().sort_values().index
 
-colors = { k:v for k,v in zip(order, ten_godisnot) }
+colors = { k:v for k,v in zip(order, sc.pl.palettes.vega_10_scanpy) }
 
-box(df_bench, x='sample', y='ARI', by='method', ax=axs[0], c=colors, hue_order=order)
-format_ax(ax=axs[0], reduced_spines=True, ylabel='ARI')
-box(df_bench, x='sample', y='NMI', by='method', ax=axs[1], c=colors, hue_order=order)
-format_ax(ax=axs[1], reduced_spines=True, ylabel='NMI')
-add_legend(label='Method', ax=axs[1], colors=colors, loc='upper left', 
-           bbox_to_anchor=(1,1), ncols=1,#round(df_bench['method'].unique().size / 2), 
-           artists_size=8, label_size=9, ticks_size=8)
+sns.stripplot(data=df_bench, x='sample', y='ARI', palette=colors.values(), 
+              hue='method', hue_order=order, size=5, ax=axs[0], dodge=True, edgecolor='k', linewidth=.5)
+axs[0].get_legend().remove()
+format_ax(ax=axs[0], xlabel='', ylabel='ARI', reduced_spines=True, xlabel_size=10, ylabel_size=10)
 
-fig.subplots_adjust(top=.9, bottom=.1, left=.1, right=.75)
-# fig.tight_layout()
-plt.show()
-fig.savefig(os.path.join(path_results, '../clonal_reconstruction_performance.png'), dpi=700)
+sns.stripplot(data=df_bench, x='sample', y='NMI', palette=colors.values(), 
+              hue='method', hue_order=order, size=5, ax=axs[1], dodge=True, edgecolor='k', linewidth=.5)
+format_ax(ax=axs[1], xlabel='', ylabel='NMI', reduced_spines=True, xlabel_size=10, ylabel_size=10)
+axs[1].get_legend().remove()
+add_legend(ax=axs[1], colors=colors, loc='upper left', bbox_to_anchor=(1,1), artists_size=10, label='Method', label_size=10, ticks_size=10)
+
+fig.subplots_adjust(right=.75, top=.85, left=.15, bottom=.15)
+fig.savefig(os.path.join(path_results, 'clonal_reconstruction_performance.png'), dpi=700)
 
 
 ##
@@ -96,79 +88,56 @@ fig.savefig(os.path.join(path_results, '../clonal_reconstruction_performance.png
 df_bench['sample'] = df_bench['sample'].astype('str')
 df_bench = df_bench.groupby(['sample', 'job_id'])['ARI'].median().sort_values().reset_index()
 top_3_jobs = df_bench.groupby('sample').apply(lambda x: x['job_id'].values[x['ARI'].argmax()]).to_dict()
-# top_3_jobs
 
 # Colors
 with open(path_colors, 'rb') as f:
     clone_colors = pickle.load(f)
 
 # Viz
-fig, axs = plt.subplots(1,5,figsize=(12,3))
+fig, axs = plt.subplots(3,4,figsize=(12,9))
 
-for i,sample in enumerate(['MDA_clones']):#  'MDA_PT', 'MDA_lung']):
+for i,sample in enumerate(['MDA_clones', 'MDA_PT', 'MDA_lung']):
 
     # Read
-    afm = sc.read(os.path.join(path_data, 'top_spaces', top_3_jobs[sample], 'afm.h5ad'))
+    afm = sc.read(os.path.join(path_data, sample, top_3_jobs[sample], 'afm.h5ad'))
     reduce_dimensions(afm)
 
-    with open(os.path.join(path_data, 'top_spaces', top_3_jobs[sample], 'bench_clonal_recontruction.pickle'), 'rb') as f:
+    with open(os.path.join(path_data, sample, top_3_jobs[sample], 'bench_clonal_recontruction.pickle'), 'rb') as f:
         d = pickle.load(f)
-    for method in d:
+
+    for method in ['MiTo', 'leiden', 'CClone']:
         labels = d[method]['labels']
-        method = method if method != 'MI_TO' else 'MiTo-UPMGA'
         afm.obs[method] = labels
         afm.obs[method][afm.obs[method].isna()] = 'unassigned'
 
     # Colors
+    n_gt = afm.obs['GBC'].unique().size
     palette = list(clone_colors.values())
-    gbc_colors,_ = assign_matching_colors(afm.obs, 'GBC', 'MiTo-UPMGA', palette)
+    gbc_colors,_ = assign_matching_colors(afm.obs, 'GBC', 'MiTo', palette)
+    afm.uns['GBC_colors'] = list(gbc_colors.values())
 
     # Plot
-    for ax,method in zip(axs,['GBC', 'MiTo-UPMGA', 'vireoSNP', 'leiden', 'CClone']):
+    for ax,method in zip(axs[i,:],['GBC', 'MiTo', 'leiden', 'CClone']):
+
         if method == 'GBC':
             colors = gbc_colors
+            title = f'{method}: n clones={n_gt}'
         else:
+            ari = d[method]['ARI']
+            n_clones = afm.obs[method].unique().size
             _,colors = assign_matching_colors(afm.obs, 'GBC', method, palette)
-        draw_embeddings(
-            afm.obs, cat=method, s=13 if sample != 'MDA_clones' else 40, 
-            ax=ax, 
-            title=f'{method} (n={afm.obs[method].unique().size})',
-            legend_kwargs={'colors':colors}, axes_kwargs={'legend':False}
+            title = f'{method}: ARI={ari:.2f}, n clones={n_clones}'
+        
+        afm.uns[f'{method}_colors'] = list(colors.values())
+        sc.pl.embedding(
+            afm, basis='X_umap', color=method, ax=ax,
+            frameon=False, save=False, legend_loc=None, show=False, 
+            size=40 if sample != 'MDA_clones' else 75,
+            title=title
         )
 
 fig.tight_layout()
-plt.show()
-# fig.savefig(os.path.join(path_results, '../viz_clonal_reconstruction.png'), dpi=500)
-
-
-##
-
-
-# Improves over current metrics
-
-# Read data
-from mito_utils.metrics import *
-
-
-sample = 'MDA_PT'
-afm = sc.read(os.path.join(path_data, 'top_spaces', top_3_jobs[sample], 'afm.h5ad'))
-reduce_dimensions(afm)
-with open(os.path.join(path_data, 'top_spaces', top_3_jobs[sample], 'bench_clonal_recontruction.pickle'), 'rb') as f:
-    d = pickle.load(f)
-
-# Get labels
-for method in d:
-    res = d[method]
-    afm.obs[method] = res['labels']
-
-df_labels = afm.obs[['GBC', 'leiden', 'vireoSNP','MI_TO', 'CClone']]
-
-np.sum(df_labels['MI_TO']=='Undefined')
-
-custom_ARI(df_labels.query('MI_TO != "Undefined"')['GBC'], df_labels.query('MI_TO != "Undefined"')['MI_TO'])
-custom_ARI(df_labels.loc[lambda x: ~x['vireoSNP'].isna()]['GBC'], df_labels.loc[lambda x: ~x['vireoSNP'].isna()]['vireoSNP'])
-normalized_mutual_info_score(df_labels.query('MI_TO != "Undefined"')['GBC'], df_labels.query('MI_TO != "Undefined"')['MI_TO'])
-normalized_mutual_info_score(df_labels.loc[lambda x: ~x['vireoSNP'].isna()]['GBC'], df_labels.loc[lambda x: ~x['vireoSNP'].isna()]['vireoSNP'])
+fig.savefig(os.path.join(path_results, 'viz_clonal_reconstruction.png'), dpi=500)
 
 
 ##

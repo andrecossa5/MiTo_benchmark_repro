6d186acd8f2873747e83e5776388d57cbe1c7c1e
Andrea Cossa <andrea.cossa05@gmail.com>
2025-10-02 15:35:04 +0200

    Fig2 .pdf reproduced

--- code/Fig2/Fig_2.py
+++ code/Fig2/Fig_2.py
@@ -42,12 +42,12 @@ with open(os.path.join(path_colors, 'clones_colors_sc.pickle'), 'rb') as f:
 
 # Clonal composition: only final QC ('filter2' on MT modality) cells, before MT-SNVs filtering. 
 samples = ['MDA_clones', 'MDA_PT', 'MDA_lung']
-path_afms = os.path.join(path_main, 'data', 'general', 'AFMs', 'maegatk')
+path_afms = os.path.join(path_main, 'data', 'general', 'AFMs')
 
 # Perform cell filtering (filter2), retrieve cell-clone assignment
 L = []
 for sample in samples:
-    afm = sc.read(os.path.join(path_afms, f'afm_{sample}.h5ad'))
+    afm = sc.read(os.path.join(path_afms, sample, f'afm_unfiltered.h5ad'))
     afm = mt.pp.filter_cells(afm, cell_filter='filter2')
     L.append(afm.obs[['sample', 'GBC']])
 df = pd.concat(L)
@@ -58,6 +58,7 @@ qc_cells = df.index
 ##
 
 
+# Fig 2a
 fig, axs = plt.subplots(1,3,figsize=(4,1.5), sharey=True)
 
 kwargs = {'orient':'h'}
@@ -80,42 +81,40 @@ plu.bar(df_, x='SH', y='sample', ax=axs[2], color='#105D62', with_label=True, fm
 plu.format_ax(xlabel='Shannon Entropy', ylabel='', ax=axs[2], reduced_spines=True)
 
 fig.tight_layout()
-fig.savefig(os.path.join(path_figures, 'clonal_numbers.pdf'))
+fig.savefig(os.path.join(path_figures, 'Fig_2a.pdf'))
 
 
 ##
 
 
+# Fig 2b
 fig, ax = plt.subplots(figsize=(4,1.5))
-
 plu.bb_plot(df, 'sample', 'GBC', categorical_cmap='tab10', ax=ax)
 fig.tight_layout()
-
-fig.savefig(os.path.join(path_figures, 'clonal_composition.pdf'))
+fig.savefig(os.path.join(path_figures, 'Fig_2b.pdf'))
 
 
 ##
 
 
+# Fig 2c
 fig, axs = plt.subplots(1,3,figsize=(6,2),subplot_kw={'projection': 'polar'})
 
-path_coverage = os.path.join(path_main, 'data', 'general', 'coverage', 'maegatk')
-
 for i,sample in enumerate(samples):
-    cov = pd.read_csv(os.path.join(path_coverage, f'{sample}_coverage.txt.gz'), header=None)
+    cov = pd.read_csv(os.path.join(path_afms, sample, 'coverage.txt.gz'), header=None)
     cov.columns = ['pos', 'cell', 'n']
     cov['cell'] = cov['cell'].map(lambda x: f'{x}_{sample}')
     cov = cov.query('cell in @qc_cells')
     mt.pl.MT_coverage_by_gene_polar(cov, sample, ax=axs[i])
 
 fig.tight_layout()
-fig.savefig(os.path.join(path_figures, 'MT_coverage.pdf'))
+fig.savefig(os.path.join(path_figures, 'Fig_2c.pdf'))
 
 
 ##
 
 
-# Legend genes
+# Fig 2c legend
 fig, ax = plt.subplots(figsize=(6,2))
 
 df_mt = (
@@ -132,30 +131,21 @@ plu.add_legend(cmap, label='MT-genes', ax=ax, loc='center', frameon=True,
 ax.axis('off')
 
 fig.tight_layout()
-fig.savefig(os.path.join(path_figures, 'MT_genes.pdf'))
+fig.savefig(os.path.join(path_figures, 'Fig_2c_legend.pdf'))
 
 
 ##
 
 
-# 2. MDA_PT example showcase --------------------------------------- # 
+# 2. nf-MiTo example (MDA_PT sample) --------------------------------------- # 
 
-# Params
-plu.set_rcParams({'figure.dpi':500})
-# plt.rcParams
-
-# See also explore output. ./results/others/explore_final_jobs
-path_afms = os.path.join(path_main, 'data', 'general', 'AFMs', 'maegatk')
-path_coverage = os.path.join(path_main, 'data', 'general', 'coverage', 'maegatk')
-path_data = os.path.join(path_main, 'data', 'lineage_inference', 'UPMGA')
-
-# Choose MT-SNVs space
+# Choose example nf-MiTo ourtput / filtered MT-SNVs space
 sample = 'MDA_PT'
 job_id = 'a4a3ca88dd'
-path_data = os.path.join(path_data, sample, job_id)
+path_data = os.path.join(path_main, 'data', 'lineage_inference', 'UPMGA', sample, job_id)
 
-# Load: afm, tree, metrics, phylocorr
-afm_unfiltered = sc.read(os.path.join(path_afms, f'afm_{sample}.h5ad'))
+# Load: afm, tree
+afm_unfiltered = sc.read(os.path.join(path_afms, sample, 'afm_unfiltered.h5ad'))
 afm = sc.read(os.path.join(path_data, 'afm_filtered.h5ad'))
 with open(os.path.join(path_data, 'annotated_tree.pickle'), 'rb') as f:
     tree = pickle.load(f)
@@ -164,9 +154,7 @@ with open(os.path.join(path_data, 'annotated_tree.pickle'), 'rb') as f:
 ##
 
 
-# Viz
-
-# Cell and variant filtering
+# Fig 2d,e,f
 fig, axs = plt.subplots(1,3,figsize=(6,2))
 
 # Cell filter
@@ -193,23 +181,22 @@ mt.pl.plot_ncells_nAD(afm_unfiltered[afm.obs_names], ax=axs[2], xticks=xticks, s
 mt.pl.plot_ncells_nAD(afm_unfiltered[afm.obs_names,afm.var_names], ax=axs[2], xticks=xticks, s=3, color='#105D62')
 
 fig.tight_layout()
-fig.savefig(os.path.join(path_figures, 'cell_variant_filters.pdf'))
+fig.savefig(os.path.join(path_figures, 'Fig_2def.pdf'))
 
 
 ##
 
 
-# Mut signature
+# Fig 2g
 fig = mt.pl.mut_profile(afm.var_names, figsize=(4,1.8), legend_kwargs={'artists_size':6, 'ticks_size':6, 'label_size':6})
-fig.savefig(os.path.join(path_figures, 'mut_profile.pdf'))
+fig.savefig(os.path.join(path_figures, 'Fig_2g.pdf'))
 
 
 ##
 
 
-# Mut spatial organization
-path_coverage = os.path.join(path_main, 'data', 'general', 'coverage', 'maegatk')
-cov = pd.read_csv(os.path.join(path_coverage, f'{sample}_coverage.txt.gz'), header=None)
+# Fig 2h
+cov = pd.read_csv(os.path.join(path_afms, sample, 'coverage.txt.gz'), header=None)
 cov.columns = ['pos', 'cell', 'n']
 cov['cell'] = cov['cell'].map(lambda x: f'{x}_{sample}')
 cov = cov.query('cell in @afm.obs_names')
@@ -218,74 +205,24 @@ fig, ax = plt.subplots(figsize=(2,2),subplot_kw={'projection': 'polar'})
 mt.pl.MT_coverage_polar(cov, var_subset=afm.var_names, ax=ax, 
                         xlabel_size=6, ylabel_size=8,
                         kwargs_subset={'markersize':2, 'c':'#105D62', 'marker':'o', 'alpha':.7})
-
 ax.grid(True, linewidth=.3)
 fig.tight_layout()
-fig.savefig(os.path.join(path_figures, 'mut_across_MT_genome.pdf'))
+fig.savefig(os.path.join(path_figures, 'Fig_2h.pdf'))
 
 
 ##
 
 
-# Variants and distances heatmap
-fig, axs = plt.subplots(1,2,figsize=(6,3))
-
-mt.pl.heatmap_variants(afm, tree=tree, ax=axs[0], cmap='afmhot_r', kwargs={'x_names_size':3})
-mt.pl.heatmap_distances(afm, tree=tree, ax=axs[1])
-
-fig.tight_layout()
-fig.savefig(os.path.join(path_figures, 'heatmaps.pdf'))
-
-
-##
-
-
-# Tree
-fig, ax = plt.subplots(figsize=(4.5,4.5))
-cmaps = { 
-    'GBC' : clones_colors,
-    'MiTo clone' : plu.create_palette(tree.cell_meta, 'MiTo clone', col_list=sc.pl.palettes.default_102)
-}
-clonal_nodes = tree.cell_meta['lca'].unique()[1:]
-mt.pl.plot_tree(
-    tree, 
-    features=['GBC', 'MiTo clone'], 
-    categorical_cmaps=cmaps, 
-    ax=ax, 
-    colorstrip_width=5,
-    internal_node_subset=clonal_nodes,
-    feature_internal_nodes='support',
-    internal_node_kwargs={'markersize':5}
-)
-tree_stats = mt.tl.get_internal_node_stats(tree)
-plu.add_cbar(
-    tree_stats['support'], palette='Spectral_r', ax=ax, 
-    vmin=.5, vmax=.8, label='Support', 
-    layout=( (1-.27,.05,.22,.02), 'bottom', 'horizontal' )
-)
-
-fig.subplots_adjust(top=.9, bottom=.1, left=.1, right=.9)
-fig.savefig(os.path.join(path_figures, f'tree.pdf'))
-plt.close()
-
-
-##
-
-
-# Enriched variants
-
-# UMAP
+# Fig 2i
 mt.pp.reduce_dimensions(afm, metric='jaccard')
 
 # Main UMAP
-fig, ax = plt.subplots(figsize=(4,4))
-mt.pl.draw_embedding(afm, feature='GBC', ax=ax, categorical_cmap=clones_colors)
-fig.tight_layout()
-fig.savefig(os.path.join(path_figures, f'embeddings.pdf'))
-
+# fig, ax = plt.subplots(figsize=(4,4))
+# mt.pl.draw_embedding(afm, feature='GBC', ax=ax, categorical_cmap=clones_colors)
+# fig.tight_layout()
+# plt.show()
 
 ##
-
 
 # Select clones and variants
 mapping = {
@@ -309,7 +246,59 @@ for i,clone in enumerate(mapping):
     plu.add_cbar(afm[:,mut].X.A.flatten(), palette='afmhot_r', ax=axs[i,1], label='AF')
 
 fig.tight_layout()
-fig.savefig(os.path.join(path_figures, f'enriched_muts.pdf'))
+fig.savefig(os.path.join(path_figures, 'Fig_2i.pdf'))
+
+
+##
+
+
+# Fig 2l,m
+fig, axs = plt.subplots(1,2,figsize=(6,3))
+mt.pl.heatmap_variants(afm, tree=tree, ax=axs[0], cmap='afmhot_r', kwargs={'x_names_size':3})
+mt.pl.heatmap_distances(afm, tree=tree, ax=axs[1])
+fig.tight_layout()
+fig.savefig(os.path.join(path_figures, 'Fig_2lm.pdf'))
+
+
+##
+
+
+# Fig 2n
+fig, ax = plt.subplots(figsize=(4.5,4.5))
+
+tree.cell_meta['MiTo clone'] = tree.cell_meta['MiTo clone'].astype('str')
+tree.cell_meta.loc[tree.cell_meta['MiTo clone'] == 'nan', 'MiTo clone'] = 'unassigned'
+cmaps = { 
+    'GBC' : clones_colors,
+    'MiTo clone' : plu.create_palette(
+        tree.cell_meta, 'MiTo clone', col_list=sc.pl.palettes.default_102, add_na=True
+    )
+}
+clonal_nodes = tree.cell_meta['lca'].unique()[1:]
+
+mt.pl.plot_tree(
+    tree, 
+    features=['GBC', 'MiTo clone'], 
+    categorical_cmaps=cmaps, 
+    ax=ax, 
+    colorstrip_width=5,
+    internal_node_subset=clonal_nodes,
+    feature_internal_nodes='support',
+    internal_node_kwargs={'markersize':5}
+)
+
+tree_stats = mt.ut.get_internal_node_stats(tree)
+plu.add_cbar(
+    tree_stats['support'], palette='Spectral_r', ax=ax, 
+    vmin=.5, vmax=.8, label='Support', 
+    layout=( (1-.27,.05,.22,.02), 'bottom', 'horizontal' )
+)
+
+fig.subplots_adjust(top=.9, bottom=.1, left=.1, right=.9)
+fig.savefig(os.path.join(path_figures, 'Fig_2n.pdf'))
+
+# Close all figures
+plt.close()
 
 
 ##
